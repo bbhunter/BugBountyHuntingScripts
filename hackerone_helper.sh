09ee4cdc7b722cc90795ba8537e6c1f4f082376a
pdelteil@gmail.com <pdelteil@gmail.com>
2024-07-13 01:09:51 +0000

    removing slashes

--- hackerone_helper.sh
+++ hackerone_helper.sh
@@ -17,12 +17,12 @@ H1_getScopefromCsv()
         program=$(echo $input | sed 's/.*teams\/\([^\/]*\)\/assets.*/\1/')
         #echo "program $program"
         #wget -q "$input" -O "$program-scope.csv"
-        curl -s $input | awk -F',' '$5 == "true" {print}' |grep 'URL\|WILDCARD'|getField 1 ","|sed 's/https:\/\///g; s/http:\/\///g'|flatten
+        curl -s $input | awk -F',' '$5 == "true" {print}' |grep 'URL\|WILDCARD'|getField 1 ","|sed 's/https:\/\///g; s/http:\/\///g'|tr -d '/'|flatten
     else
         #checking if file exists 
         if [[ -f "$input" ]]; then
             #echo "$input exists."
-            awk -F',' '$4 == "true" {print}' "$input"|grep 'URL\|WILDCARD'|getField 1 ","|sed 's/https:\/\///g; s/http:\/\///g'|flatten
+            awk -F',' '$4 == "true" {print}' "$input"|grep 'URL\|WILDCARD'|getField 1 ","|sed 's/https:\/\///g; s/http:\/\///g'|tr -d '/'| flatten
         else
             echo "$input not found!"
         fi
